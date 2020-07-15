4e9e5e82bf4d6b680e3d0bd4b32ae54f0c1f50df
Pat McClernan <patmccler@gmail.com>
2020-07-15 15:40:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+ function map(array, callback) {
+  const result = []
+  for(const elem of array) {
+    result.push(callback(elem))
+  }
+  return result
+ }
+
+ function reduce(array, callback, start) {
+  let result = start ? start : array[0]
+  let i = start ? 0 : 1
+  for(const elem of array.slice(i)) {
+    result = callback(elem, result)
+  }
+  return result
+ }
