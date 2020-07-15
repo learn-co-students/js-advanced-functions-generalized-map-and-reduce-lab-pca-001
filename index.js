109b65f2bb6ece91366a6476085ad16dedff99a8
Andrew Kloecker <lt.kvothe@gmail.com>
2020-07-15 15:45:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+const map = (arr, fcn) => {
+  const ans = [];
+  arr.forEach( el => ans.push(fcn(el)) );
+  return ans;
+}
+
+const reduce = (arr, fcn, start=0) => {
+  let e = fcn(true, true) === true ? true : start;
+  arr.forEach( a => e = fcn(e,a));
+  return e;
+}
