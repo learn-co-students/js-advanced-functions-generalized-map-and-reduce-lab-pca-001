1e433e953f049369da1184f359f1247aca827629
Madison McCardle <madison.mccardle@powerhrg.com>
2020-07-15 21:43:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your functions here
+function map(sourceArray, callBackfunc) {
+  let newArray = []
+  for (let index = 0; index < sourceArray.length; index++) {
+    let something = sourceArray[index]
+    newArray.push(callBackfunc(something))
+  }
+  return newArray
+}
+
+function reduce(sourceArray, callBackfunc, startingPoint){
+  // if (!!startingPoint) {
+  //   let newArray = startingPoint
+  // } else {
+  //   let newArray = sourceArray[0]
+  // }
+  // if (!!startingPoint) {
+  //   let index = 0
+  // } else {
+  //   let index = 1
+  // }
+  let newArray = (!!startingPoint) ? startingPoint : sourceArray[0]
+  let index = (!!startingPoint) ? 0 : 1
+  for (const element of sourceArray.slice(index)) {
+    newArray = callBackfunc(element, newArray)
+  }
+  return newArray
+}
