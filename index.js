4bd39e57460d692160f153921987eca9864ab758
Aaron Wilson <adub65@gmail.com>
2020-07-17 14:37:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(sourceArray, callback) {
+  const result = []
+  for(const element of sourceArray) {
+    result.push(callback(element))
+  }
+  return result
+ }
+
+ function reduce(array, callback, start) {
+  let result = start ? start : array[0]
+  let i = start ? 0 : 1
+  for(const elem of array.slice(i)) {
+    result = callback(elem, result)
+  }
+  return result
+ }
